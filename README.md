202126413573f15d2cdd13e42663b62d28d14174
harsh8601 <22je0638@iitism.ac.in>
2024-11-13 02:27:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a chat app built using the MERN stack.
+The app enables real-time communication.
+Link:https://chat-app-gu0o.onrender.com/login
+
+
+
+
---
+++ backend/server.js
@@ -0,0 +1,69 @@
+/*
+import express from "express";
+import dotenv from "dotenv";
+//import cookieParser from 'cookie-parser'
+
+//import authRoutes from './routes/auth.routes.js'
+//import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+import connectToMongoDB from './db/connectToMongoDB.js'
+//import { app, server } from './socket/socket.js'
+const app=express();
+
+const PORT = process.env.PORT || 5000;
+dotenv.config();
+//const __dirname = path.resolve()
+
+app.use(express.json()) //parse the incoming json
+//app.use(cookieParser())
+
+app.use('/api/auth', authRoutes)
+//app.use('/api/message', messageRoutes)
+//app.use('/api/users', userRoutes)
+
+//app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+/*app.get("/", (req, res) => {
+  res.sendFile("HEllo world");
+})
+app.use('/api/auth', authRoutes);
+app.listen(PORT, () => {
+  connectToMongoDB()
+  console.log(`Server running on port ${PORT}`)
+})
+*/
+import path from 'path'
+import express from 'express'
+import 'dotenv/config'
+import cookieParser from 'cookie-parser'
+
+import authRoutes from './routes/auth.routes.js'
+import messageRoutes from './routes/message.routes.js'
+import userRoutes from './routes/user.routes.js'
+
+import connectToMongoDB from './db/connectToMongoDB.js'
+import { app, server } from './socket/socket.js'
+
+const PORT = process.env.PORT
+
+const __dirname = path.resolve()
+
+app.use(express.json()) //parse the incoming json
+app.use(cookieParser())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/message', messageRoutes)
+app.use('/api/users', userRoutes)
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
+
+server.listen(PORT, () => {
+  connectToMongoDB()
+  console.log(`Server running on port ${PORT}`)
+})
+
